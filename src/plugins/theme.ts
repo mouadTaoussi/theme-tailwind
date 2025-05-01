30bef7e2267684bdd81539cc7cfde36e772ef714
Mouad Taoussi <50294614+mouadTaoussi@users.noreply.github.com>
2025-05-01 13:26:53 +0200

    project theme-tailwind

---
+++ src/plugins/theme.ts
@@ -0,0 +1,14 @@
+// import type { OrugaOptions, ComponentProps } from "@oruga-ui/oruga-next";
+// import { isTrueish } from "@oruga-ui/oruga-next";
+
+// import theme style
+// import "../assets/scss/bulma-build.scss";
+
+const tailwindConfig = {
+    name: "Test",
+    componentName: {
+        interneprops: "classe name",
+    },
+};
+
+export { tailwindConfig as config };
